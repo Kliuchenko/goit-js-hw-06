7568f9d4147a41515fb3bfc40ab59d301a81211c
Kliuchenko <pasha.kluchenko@gmail.com>
2022-12-14 20:05:29 +0200

    complete 2nd task

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingredientsList = document.querySelector('#ingredients');
+// console.log(ingredientsList);
+
+for (const item of ingredients) {
+  const items = document.createElement('li')
+  items.classList = 'item';
+  items.textContent = item;
+  ingredientsList.append(items);
+  // console.log(items);
+}
